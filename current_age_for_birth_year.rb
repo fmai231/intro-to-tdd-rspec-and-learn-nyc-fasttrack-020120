04caba38210bc2c043c1cead8052c909ddb471aa
Franky Mai <frankymai91@gmail.com>
2020-02-01 17:48:56 +0000

    Automatically backed up by Learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -1,5 +1,5 @@
-def current_age_for_birth_year(birth_year)
-  return 2003 - birth_year
+def current_age_for_birth_year(birthyear)
+  return 2003 - birthyear
 end
 
 age_of_person = current_age_for_birth_year(1984)
